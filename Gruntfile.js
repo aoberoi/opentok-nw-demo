b79c98aa31530d47079704cd16ed3d590cfb6155
Ankur Oberoi <aoberoi@gmail.com>
2014-04-09 23:55:11 -0700

    disables linux builds, grunt plugin might not handle this well

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,8 +5,8 @@ module.exports = function(grunt) {
           build_dir: './webkitbuilds', // Where the build version of my node-webkit app is saved
           mac: true, // We want to build it for mac
           win: true, // We want to build it for win
-          linux32: true, // We don't need linux32
-          linux64: true, // We don't need linux64
+          linux32: false, // We don't need linux32
+          linux64: false, // We don't need linux64
           zip: false
       },
       src: ['./app/**/*'] // Your node-webkit app
